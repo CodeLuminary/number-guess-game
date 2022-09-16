9d5f1f10cca31bf11c1d29192732e4efcc559201
CodeLuminary <victorijoni@yahoo.com>
2022-09-16 08:53:11 +0100

    Settings updated

--- game/src/components/Setting.js
+++ game/src/components/Setting.js
@@ -1,15 +1,40 @@
-import {memo} from "react";
+import { memo, useState} from "react";
+import style from "../css/setting.module.css";
+import {randomInteger} from "../helperFunctions/Game";
+import {useDispatch} from "react-redux";
+import {setGame} from "../redux/reducers/gameReducer"
 
-const Setting = ()=>{
-    return (
-        <>
-            <label>Lower</label><br/>
-            <input type="text"/><br/>
-            <label>Upper</label><br/>
-            <input type="text"/>
-            <button>Reset</button>
-        </>
-    )
-}
+const Setting = ({ shouldShow, closeSetting }) => {
+  const dispatch = useDispatch();
+  const [lowerValue, setLowerValue] = useState(1);
+  const [upperValue, setUpperValue] = useState(10);
+  console.log("great");
+  return (
+    <div
+      style={{ display: shouldShow ? `flex` : `none` }}
+      className={style.content}
+    >
+      <div>
+        <button onClick={closeSetting} className={style.cancel}>
+          X
+        </button>
+        <label>Set the lower & upper limit of the guess number</label><br/><br/>
+        <label>Lower</label>
+        <br />
+        <input onInput={e=>setLowerValue(Number(e.target.value))} type="text" placeholder="Enter lower limit"/>
+        <br />
+        <label>Upper</label>
+        <br />
+        <input onInput={e=>setUpperValue(Number(e.target.value))} type="text" placeholder="Enter upper limit"/>
+        <button onClick={()=>{dispatch(setGame({
+          guessValue: randomInteger(lowerValue, upperValue),
+          lowerBound: lowerValue,
+          upperBound: upperValue
+        }));
+        alert("Settings saved successfully")}} className={style.reset}>Reset</button>
+      </div>
+    </div>
+  );
+};
 
 export default memo(Setting);
